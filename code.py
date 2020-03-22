2609108add508890378f8b42a9a54059d9f7c9c4
AhmedEhabH <ahmed.ehab.h@gmail.com>
2020-03-22 15:41:25 +0200

    rename with number and dash and name

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import os
 
-path = '/media/root/1E5C4BD15C4BA27F/world/downloads/music/Test' #Path
+path = '/media/ahmed/New Volume/World/Quran/Naser El Qatami' #Path
 # print(os.listdir(path))
 names = ['lec 04', 'lec 05', 'lec 06', 'lec 07', 'lec 08'] #List of new name
 for i, filename in enumerate(os.listdir(path)):
